8a4c98a1675b77c38ba0db6d5d0b8c0a06117602
agegnew <53579837+agegnew@users.noreply.github.com>
2019-08-02 01:16:06 +0300

    first website upload

---
+++ src/App.vue
@@ -0,0 +1,136 @@
+<template>
+  <div id="app">
+
+    <nav class="navbar has-navbar-fixed-top" role="navigation" aria-label="main navigation">
+      <div class="navbar-brand navbar-right">
+        <a class="navbar-item" href="">
+          <img style="padding: 0;margin: -1em;" src="./assets/logos.png" width="112" height="1000">
+          <h1 style="padding: 0;margin: -1em;">Vue Sample</h1>
+        </a>
+
+        <a role="button" class="navbar-burger burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample" v-on:click="toggleNav" v-bind:class="{ 'is-active' : isActive}">
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+        </a>
+      </div>
+
+      <div id="navbarBasicExample" class="navbar-menu navbar-left" v-bind:class="{ 'is-active' : isActive}">
+        <div class="navbar-start">
+          <router-link to="/" class="navbar-item r-item">Home</router-link>
+          <router-link to="faq" class="navbar-item r-item">Features</router-link>
+          
+        
+          <router-link to="faq" class="navbar-item r-item">More</router-link>
+
+            
+          
+          
+          <router-link to="faq" class="navbar-item r-item">About</router-link> 
+        </div>
+
+        <div class="navbar-end">
+          <div class="navbar-item">
+            <div class="control">
+              <a class="button is-primary is-outlined">
+                <strong>Sign up</strong>
+              </a>
+              <a class="button is-light is-outlined">
+                Log in
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </nav>
+
+    <router-view/>
+
+    
+
+    <footer class="footer is-primary">
+      <div class="container">
+        <div class="columns">
+          <div class="column">
+            <p>And this is all the accounts that u can contact us!</p>
+          </div>
+          <div class="column has-text-right">
+            <a class="icon" href="#"><i class="fa fa-facebook"></i></a>
+            <a class="icon" href="#"><i class="fa fa-twitter"></i></a>
+          </div>
+        </div>
+      </div>
+
+
+    </footer>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  data: function(){
+    return{
+      isActive: false
+    }
+  },
+  methods:{
+    toggleNav: function(){
+      this.isActive=!this.isActive;
+    }
+  }
+
+}
+</script>
+
+<style lang="sass">
+@import '../node_modules/bulma/bulma.sass'
+@import 'mq.sass'
+
+.navbar
+  background-color: #383838 !important
+  border-bottom: solid 2px red
+  padding: 0 1em
+  padding-top: 1em
+   
+  a:hover
+    color: black  !important
+  a
+    color: darkgray !important
+  h1
+    color: #fff
+  .navbar-left
+    +desktop
+      margin-left: 30em !important
+    +mobile
+      background-color: #383838 !important
+    +tablet
+      background-color: #383838 !important
+  .r-item
+    color: #c1c1c1
+    padding: 0.3rem 1.9rem
+    +mobile
+      color: white !important
+    
+    &:hover
+      background-color: gray !important 
+       
+  .navbar-burger span
+    background-color: #c1c1c1
+    color: #fff
+
+footer
+  background-color: $primary !important
+  color: #fff
+  .icon
+    color: #fff !important
+    margin-left: 10px
+
+  // .navbar-start
+  //   color:#fff
+  //   font-weigt: bold  !important
+    
+
+
+</style>
---
+++ src/components/Home.vue
@@ -0,0 +1,126 @@
+<template>
+  <div class="home">
+    <section class="hero is-medium is-primary is-bold">
+      <div class="hero-body">
+        <div class="container column is-two-thirds">
+          <h1 class="title">
+            Welcome to my Sample vue.js project
+          </h1>
+          <h2 class="subtitle is-4">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus, 
+            iure inventore vero debitis dignissimos rerum similique hic quia animi accusamus numquam 
+            nam reprehenderit aliquid at assumenda, fugit a fugiat necessitatibus.
+          </h2>
+          <a class="button is-large is-primary" id="learn">Learn more</a>
+
+        </div>
+
+      </div>
+    </section>
+
+    <section class="section">
+      <div class="container columns">
+        <div class="column is-1 has-text-centered">
+          <i class="fa fa-cog is-primary"></i>
+        </div>
+        <div class="column is-one-third-desktop">
+          <p class='title'>
+            <strong>we give services that you won't get anywhere else</strong>
+          </p>
+
+        </div>
+        <div class="column">
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Tenetur architecto illum fuga. Porro, optio at debitis eligendi doloremque cum obcaecati 
+            praesentium consequuntur repellat possimus, laboriosam enim ad consequatur libero velit! 
+          </p>
+
+        </div>
+      </div>
+    <div class="columns pd">
+      <div class="column">
+        <div class="card">
+          <div class="card-content">
+            <p class="title">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi expedita 
+                            eaque atque repudiandae vel harum accusantium 
+                            
+            </p>
+            <p class="subtitle"> -Agegnew Mersha</p>
+          </div>
+        </div>
+      </div>
+      <div class="column">
+        <div class="card"> 
+          <div class="card-content">
+            <p class="title">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi expedita 
+                            eaque atque repudiandae vel harum accusantium 
+                            
+            </p>
+            <p class="subtitle"> -Agegnew Mersha</p>
+          </div>
+        </div>
+      </div>
+      <div class="column">
+        <div class="card">
+          <div class="card-content">
+            <p class="title">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi expedita 
+                            eaque atque repudiandae vel harum accusantium 
+                           
+            </p>
+            <p class="subtitle"> -Agegnew Mersha</p>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    </section>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'home',
+  
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang='sass' scoped>
+@import '../mq.sass'
+
+
+  
+.hero
+  //
+  background-image: linear-gradient(to bottom, rgba(2, 30, 83, 0.808), rgba(3, 119, 134, 0.808)),url('../assets/background.jpg') !important
+  background-size: cover
+
+  .title
+   
+    +mobile
+      font-weight: bold
+    +tablet
+      font-size: 2.5rem
+    +desktop
+      margin-left: -150px
+      font-size: 4rem
+      margin-top: 2rem
+  
+
+h2
+  margin:1.5rem 0 2rem 0 !important
+
+.fa-cog
+  font-size: 40px
+  color: rgb(50, 255, 84)
+    
+.pd
+  background-color: rgba(4, 155, 155, 0.493)
+
+  +tablet
+    padding: 2em 0
+
+  
+
+</style>
